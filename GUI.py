1a198425b0185ffa2a634e23be95b67d46930f64
djekiccaar <94829956+djekiccaar@users.noreply.github.com>
2021-12-16 16:32:01 +0100

    Drugi

--- GUI.py
+++ GUI.py
@@ -1,15 +1,30 @@
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 from tabula import read_pdf, convert_into
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
+import PyPDF2
+import re
 
 
+filename = PyPDF2.PdfFileReader(r"VF505_V3_R7_P520MY.pdf")
+NumPages = filename.getNumPages()
+String = "Ignition Working Conditions Table"
+
+def Pretraga(filename):
+    NumPages = filename.getNumPages()
+    for i in range(0, NumPages):
+        PageObj = filename.getPage(i)
+        Text = PageObj.extractText()
+        ResSearch = re.search(String, Text)
+        if ResSearch != None:
+            print(ResSearch)
+            a=i+1
+            print("Page Number = " + str(a))
+    return a
 
 #list all tables
-tables = read_pdf(filename, pages='17')
+pages=Pretraga(filename)
+print(pages)
+tables = read_pdf(r"VF505_V3_R7_P520MY.pdf", pages=str(pages))
 
 table_number =1
 
--- Pretraga.py
+++ Pretraga.py
@@ -1,18 +1,21 @@
 import PyPDF2
 import re
 
-# Open the pdf file
+# open the pdf file
 object = PyPDF2.PdfFileReader(r"VF505_V3_R7_P520MY.pdf")
 
-# Get number of pages
+# get number of pages
 NumPages = object.getNumPages()
 
-# Enter code here
+# define keyterms
 String = "Ignition Working Conditions Table"
 
-# Extract text and do the search
+# extract text and do the search
 for i in range(0, NumPages):
     PageObj = object.getPage(i)
     Text = PageObj.extractText()
-    if re.search(String,Text):
-         print("Pattern Found on Page: " + str(i))
+    ResSearch = re.search(String, Text)
+    if ResSearch != None:
+        print(ResSearch)
+        a=i+1
+        print("Page Number = " + str(a))
